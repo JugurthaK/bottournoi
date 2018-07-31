108ee9eb7a3eb80cf22f0248e43f70565d3991cd
JugurthaK <jugurtha.halzoun@gmail.com>
2018-07-31 13:42:10 +0200

    Utilisation du package Fortnite API, création de auth.js

--- bot.js
+++ bot.js
@@ -1,6 +1,17 @@
 const Discord = require("discord.js");
+const auth = require ("./auth");
 const bot = new Discord.Client();
-const request = require("request");
+const fortnite = require("fortnite-api");
+
+
+let fortniteAPI = new fortnite([
+    auth.fortniteEmail,
+    auth.fortnitePassword,
+    auth.fortniteCliKey,
+    auth.fortniteGameKey
+], {
+        debug: true
+    });
 
 bot.on('ready', () => {
     console.log("Bot Connecté");
@@ -19,113 +30,86 @@ bot.on('message', (message) => {
         var pseudo = args[1];
         var plateforme = args[2];
 
-
         if (pseudo == undefined || plateforme == undefined) {
             member.createDM().then(channel => {
                 return channel.send("**Vous n'avez pas spécifié de pseudonyme ou de plateforme**");
             })
-        } else {
+        }
 
-            var options = {
-                method: "GET",
-                url: `https://fortnite.y3n.co/v2/player/${pseudo}`,
-                headers: {
-                    'User-Agent': 'node js request',
-                    'X-Key': 'GvHZrXutfChSYUyzS9jt'
-                }
-            }
+        else {
+            fortniteAPI.login().then(() => {
+                if (fortniteAPI.checkPlayer(pseudo, plateforme)) {
+                    fortniteAPI.getStatsBR(pseudo, plateforme, "alltime")
+                        .then(stats => {
+                            let role = member.guild.roles.find("name", plateforme.toUpperCase());
+                            member.addRole(role).catch(console.error);
 
-            request(options, (error, res, body) => {
-                if (!error && res.statusCode == 200) {
-                    var stats = JSON.parse(body);
+                            if (stats.lifetimeStats.matches >= 1000) {
 
-                    // gestion des plateformes
-                    if (plateforme == "ps4") {
-                        var reqFortnite = stats.br.stats.ps4.all.kpd;
-                        var reqFortniteKills = stats.br.stats.ps4.all.matchesPlayed;
+                                if (stats.lifetimeStats['k/d'] >= 2 && stats.lifetimeStats['k/d'] <= 3.99) {
+                                    role = member.guild.roles.find('name', '+2');
+                                    member.addRole(role).catch(console.error);
+                                    member.createDM().then(channel => {
+                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                    })
+                                    member.setNickname(pseudo);
+                                    message.reply("``````Le joueur " + pseudo + " est inscrit au tournoi !``````");
 
-                        var role = member.guild.roles.find('name', 'PS4');
-                        member.addRole(role).catch(console.error);
-                    } else if (plateforme == "pc") {
-                        var reqFortnite = stats.br.stats.pc.all.kpd;
-                        var reqFortniteKills = stats.br.stats.pc.all.matchesPlayed;
+                                } else if (stats.lifetimeStats['k/d'] >= 4 && stats.lifetimeStats['k/d'] <= 5.99) {
+                                    role = member.guild.roles.find('name', '+4');
+                                    member.addRole(role).catch(console.error);
+                                    member.createDM().then(channel => {
+                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                    })
+                                    member.setNickname(pseudo);
+                                    message.reply("```Le joueur " + pseudo + " est inscrit au tournoi !```");
 
-                        var role = member.guild.roles.find('name', 'PC');
-                        member.addRole(role).catch(console.error);
+                                } else if (stats.lifetimeStats['k/d'] >= 6 && stats.lifetimeStats['k/d'] <= 7.99) {
+                                    role = member.guild.roles.find('name', '+6');
+                                    member.addRole(role).catch(console.error);
+                                    member.createDM().then(channel => {
+                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                    })
+                                    member.setNickname(pseudo);
+                                    message.reply("```Le joueur " + pseudo + " est inscrit au tournoi !```");
 
-                    } else if (plateforme == "xb1") {
-                        var reqFortnite = stats.br.stats.xb1.all.kpd;
-                        var reqFortniteKills = stats.br.stats.xb1.all.matchesPlayed;
-                        var role = member.guild.roles.find('name', 'XB1');
-                        member.addRole(role).catch(console.error);
-                    } else {
-                        member.createDM().then(channel => {
-                            return channel.send("La **plateforme selectionnée est invalide**, je vous rappelle que les trois choix sont pc / ps4 / xb1");
+                                } else if (stats.lifetimeStats['k/d'] >= 8) {
+                                    role = member.guild.roles.find('name', '+8');
+                                    member.addRole(role).catch(console.error);
+                                    member.createDM().then(channel => {
+                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                    })
+                                    member.setNickname(pseudo);
+                                    message.reply("```Le joueur " + pseudo + " est inscrit au tournoi !```");
+
+                                } else {
+                                    member.createDM().then(channel => {
+                                        return channel.send("**Désolé, votre KDA est trop bas pour participer à ce tournoi**");
+                                    })
+                                    member.setNickname(pseudo);
+                                }
+
+                            } else {
+                                member.createDM().then(channel => {
+                                    return channel.send("**Désolé vous n'avez pas joué suffisamment de games pour participer à ce tournoi**");
+                                })
+                            }
                         })
-                    }
-
-                    // gestion des kills & KDA
-                    if (reqFortniteKills >= 1000) {
-                        if (reqFortnite >= 2 && reqFortnite <= 3.99) {
-                            var role = member.guild.roles.find('name', '+2');
-                            member.addRole(role).catch(console.error);
+                        .catch(err => {
                             member.createDM().then(channel => {
-                                return channel.send("**Vos rôles ont été ajoutés**");
+                                return channel.send("*Plateforme ou pseudo invalide*");
                             })
-                            member.setNickname(pseudo);
-                            message.reply("Le joueur "+pseudo+" est inscrit au tournoi !");
-                        } else if (reqFortnite >= 4 && reqFortnite <= 5.99) {
-                            var role = member.guild.roles.find('name', '+4');
-                            member.addRole(role).catch(console.error);
-                            member.createDM().then(channel => {
-                                return channel.send("**Vos rôles ont été ajoutés**");
-                            })
-                            member.setNickname(pseudo);
-                            message.reply("Le joueur "+pseudo+" est inscrit au tournoi !");
-                        } else if (reqFortnite >= 6 && reqFortnite <= 7.99) {
-                            var role = member.guild.roles.find('name', '+6');
-                            member.addRole(role).catch(console.error);
-                            member.createDM().then(channel => {
-                                return channel.send("**Vos rôles ont été ajoutés**");
-                            })
-                            member.setNickname(pseudo);
-                            message.reply("Le joueur "+pseudo+" est inscrit au tournoi !");
-                        } else if (reqFortnite >= 8) {
-                            var role = member.guild.roles.find('name', '+8');
-                            member.addRole(role).catch(console.error);
-                            member.createDM().then(channel => {
-                                return channel.send("**Vos rôles ont été ajoutés**");
-                            })
-                            member.setNickname(pseudo);
-                            message.reply("Le joueur "+pseudo+" est inscrit au tournoi !");
-                        } else {
-                            member.createDM().then(channel => {
-                                return channel.send("**Désolé, votre KDA est trop bas pour participer à ce tournoi**");
-                            })
-                            member.setNickname(pseudo);
-                        }
-                    } else {
-                        member.createDM().then(channel => {
-                            return channel.send("**Désolé vous n'avez pas joué suffisamment de games pour participer à ce tournoi**");
-                        })
-                    }
-                } else {
-                    member.createDM().then(channel => {
-                        channel.send("Désolé une erreur s'est produite, contactez l'administrateur avec ce code: " + res.statusCode);
-                        return channel.send("Si le code est 404, dans ce cas, vérifiez bien que vous avez renseigné **votre pseudo Epic Games** et non pas votre pseudo PSN/XB1");
-                    })
+                        });
                 }
             });
-
         }
     }
-})
+});
 
 bot.on('guildMemberAdd', (member) => {
     member.createDM().then(channel => {
-        channel.send("**Bienvenue sur le serveur** pour obtenir ton Rôle, utilise la commande **'!tournoi pseudoepicgames plateforme {pc/ps4/xb1}**' dans le channel **#bot_tournoi**");
-        return channel.send("Exemple : **!tournoi JugurthaK pc**");
+        return channel.send("**Bienvenue sur le serveur** pour obtenir ton Rôle, utilise la commande **'!tournoi pseudoepicgames plateforme {pc/ps4/xb1}**' dans le channel **#bot_tournoi** \n Exemple : **!tournoi JugurthaK pc**");
     }).catch(console.error)
 })
 
-bot.login("NDcyMzU1MjE0MTQyMjEwMDcw.DjzEUQ.ElX_5HReuEK7hoMss5GvyiI7LHg");
+bot.login(auth.discordToken);
