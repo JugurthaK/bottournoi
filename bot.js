d6ca06d593ffe6a2277793f5ded170cf4f9b4a14
JugurthaK <jugurtha.halzoun@gmail.com>
2018-08-01 10:45:50 +0200

    Ajout d'un système d'authentification grace à l'ID Discord & Fortnite

--- bot.js
+++ bot.js
@@ -1,8 +1,15 @@
 const Discord = require("discord.js");
-const auth = require ("./auth");
+const auth = require("./auth");
 const bot = new Discord.Client();
 const fortnite = require("fortnite-api");
 
+let auth_discord_ftn = [];
+let fn = key => {
+    let temp = {}
+    if ((temp = auth_discord_ftn.find(x => Object.keys(x).find(y => y === key))))
+        temp = temp[Object.keys(temp)[0]]
+    return temp
+}
 
 let fortniteAPI = new fortnite([
     auth.fortniteEmail,
@@ -17,18 +24,19 @@ bot.on('ready', () => {
     console.log("Bot Connecté");
 });
 
-bot.on('message', (message) => {
+bot.on('message', (message, user) => {
     if (message.content === 'ping') {
         message.reply('pong !')
+        console.log
     }
 });
 
 bot.on('message', (message) => {
     if (message.content.startsWith("!tournoi") && message.channel.name === "bot_tournoi") {
-        var member = message.member;
-        var args = message.content.split(" ");
-        var pseudo = args[1];
-        var plateforme = args[2];
+        let member = message.member;
+        let args = message.content.split(" ");
+        let pseudo = args[1];
+        let plateforme = args[2];
 
         if (pseudo == undefined || plateforme == undefined) {
             member.createDM().then(channel => {
@@ -43,32 +51,39 @@ bot.on('message', (message) => {
                         .then(stats => {
                             let role = member.guild.roles.find("name", plateforme.toUpperCase());
                             member.addRole(role).catch(console.error);
+                            let ftnAccountId = stats.info.accountId;
 
                             if (stats.lifetimeStats.matches >= 1000) {
+
+                                // Ajout des élements pour authentifier
+                                if (stats.lifetimeStats['k/d'] >= 2) {
+                                    auth_discord_ftn.push({ [member.id]: ftnAccountId });
+                                }
 
                                 if (stats.lifetimeStats['k/d'] >= 2 && stats.lifetimeStats['k/d'] <= 3.99) {
                                     role = member.guild.roles.find('name', '+2');
                                     member.addRole(role).catch(console.error);
                                     member.createDM().then(channel => {
-                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                        return channel.send("Vos rôles ont été ajoutés, \n Pour vous authentifier, vous devez désormais taper la commande **!auth votreidfortnite** en DM \n Si vous ne savez pas comment obtenir cet id, regardez dans le channel **#aide**");
                                     })
                                     member.setNickname(pseudo);
-                                    message.reply("``````Le joueur " + pseudo + " est inscrit au tournoi !``````");
+                                    message.reply("```Le joueur " + pseudo + " est inscrit au tournoi !```");
 
                                 } else if (stats.lifetimeStats['k/d'] >= 4 && stats.lifetimeStats['k/d'] <= 5.99) {
                                     role = member.guild.roles.find('name', '+4');
                                     member.addRole(role).catch(console.error);
                                     member.createDM().then(channel => {
-                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                        return channel.send("Vos rôles ont été ajoutés, \n Pour vous authentifier, vous devez désormais taper la commande **!auth votreidfortnite** en DM \n Si vous ne savez pas comment obtenir cet id, regardez dans le channel **#aide**");
                                     })
                                     member.setNickname(pseudo);
                                     message.reply("```Le joueur " + pseudo + " est inscrit au tournoi !```");
+
 
                                 } else if (stats.lifetimeStats['k/d'] >= 6 && stats.lifetimeStats['k/d'] <= 7.99) {
                                     role = member.guild.roles.find('name', '+6');
                                     member.addRole(role).catch(console.error);
                                     member.createDM().then(channel => {
-                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                        return channel.send("Vos rôles ont été ajoutés, \n Pour vous authentifier, vous devez désormais taper la commande **!auth votreidfortnite** en DM \n Si vous ne savez pas comment obtenir cet id, regardez dans le channel **#aide**");
                                     })
                                     member.setNickname(pseudo);
                                     message.reply("```Le joueur " + pseudo + " est inscrit au tournoi !```");
@@ -77,7 +92,7 @@ bot.on('message', (message) => {
                                     role = member.guild.roles.find('name', '+8');
                                     member.addRole(role).catch(console.error);
                                     member.createDM().then(channel => {
-                                        return channel.send("**Vos rôles ont été ajoutés**");
+                                        return channel.send("Vos rôles ont été ajoutés, \n Pour vous authentifier, vous devez désormais taper la commande **!auth votreidfortnite** en DM \n Si vous ne savez pas comment obtenir cet id, regardez dans le channel **#aide**");
                                     })
                                     member.setNickname(pseudo);
                                     message.reply("```Le joueur " + pseudo + " est inscrit au tournoi !```");
@@ -102,6 +117,33 @@ bot.on('message', (message) => {
                         });
                 }
             });
+        }
+    }
+
+    if (message.content.startsWith("!auth")) {
+        
+        if (message.channel.type === "dm") {
+            
+            let args = message.content.split(" ");
+            let role = bot.guilds.get('472356854232514560').roles.find(role => role.name === "Certifié");
+            bot.guilds.get('472356854232514560').members.get(message.author.id).addRole(role);
+            let id = args[1];
+
+            if (id === fn(message.author.id)) {
+                message.author.createDM().then(channel => {
+                    return channel.send("*Compte certifié*");
+                })
+           
+            } else {
+                message.author.createDM().then(channel => {
+                    return channel.send("**L'identifiant est erroné, si vous avez besoin d'aide, renseignez vous sur le channel '#aide**");
+                })
+            }
+        
+        } else {
+            message.author.sendMessage("Votre clé d'authentification doit être envoyé par DM, et non dans un Channel").then(() => {
+                message.delete(1000);
+            }).catch()
         }
     }
 });
